7c7e9185a7a4e28265aee1fafc5497398fdf336c
Mikubu39 <nicek92408@gmail.com>
2025-12-25 22:31:30 +0700

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,7 @@ async function bootstrap() {
   app.use(urlencoded({ extended: true, limit: '50mb' }));
   // CORS
   app.enableCors({
-    origin: ['http://localhost:5173', 'http://localhost:5174'],
+    origin: '*',
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
